191dbda894518764d03544cfbef82d699ee95255
Josquin Larsen <jsqnlrsn@gmail.com>
2026-05-30 16:19:20 -0400

    basic navbar

---
+++ app/src/components/Navbar.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    const [loggedIn, setIsLoggedIn] = useState(false);
+
+    const logout = () => {
+        setIsLoggedIn(false);
+    }
+
+    const login = () => {
+        setIsLoggedIn(true);
+    }
+
+    return (
+        <>
+        <div className="navbar">
+        <header>
+            <div>
+                <h1>Wally & Coda</h1>
+            </div>
+        </header>
+        <div>
+            <nav>
+                {loggedIn? (
+                    <>
+                    <a>Account</a>
+                    <button
+                        className="logout-btn"
+                        onClick={logout}
+                    >Logout</button>
+                    </>
+                ) : (
+                    <>
+                    <a>Account</a>
+                    <button
+                        className="logout-btn"
+                        onClick={login}
+                    >Login</button>
+                    </>
+                )}
+            </nav>
+        </div>
+        </div>
+        </>
+    )
+};
+
+export default Navbar;
